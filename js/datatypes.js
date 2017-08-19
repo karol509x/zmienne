92602b8bf33b3c119ddcd7bc1bbc11eba55cf528
karol509x <karol509x@gmail.com>
2017-08-19 10:39:17 +0200

    zmienne-nowe-wartosci

---
+++ js/datatypes.js
@@ -0,0 +1,28 @@
+'use strict'
+/* Typ liczbowy */
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+/* Typ - łańcuch znakow - string*/
+
+var wyplataStr = "5000";
+var premiaStr= "2500"
+var calkowitaWyplataStr = "Zarabiam: " + wyplataStr + " " + premiaStr;
+console.log(calkowitaWyplataStr);
+
+/* Typ logiczny - boolean */
+
+var smog = true;
+console.log(smog);
+
+
+/* Typ specjalne - undefined / null */
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
